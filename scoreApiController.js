bfa72f6bb9ad3a630cfd60cdf3acd1af43212b1d
Dim <88244781+dimtaiwo@users.noreply.github.com>
2021-10-29 13:57:49 +0100

    Update scoreApiController.js

--- scoreApiController.js
+++ scoreApiController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/scores", async (req, res) => {
   try {
-    const data = await Score.find().limit(15).sort({ points: -1 });
+    const data = await Score.find();
     res.status(200).json(data);
   } catch (error) {
     res.send("Server error " + error);
